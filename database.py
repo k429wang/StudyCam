ab8e32437c64a7d46316d87af6480a3ee22be513
sabrinapunjani <sabrinaxjp@gmail.com>
2023-01-14 13:51:57 -0500

    database first commit
    studySession class to report sessions

---
+++ database.py
@@ -0,0 +1,14 @@
+#database
+
+from time import time
+
+class StudySession:
+    def __init__(self): 
+        self.startTime  = time()
+        self.time =0
+        self.endTime
+        #can add modules here
+    
+    def finished(self):
+        self.endTime = time()
+        self.time = self.endTime -self.startTime 
