ffe6952ec815cb5807b9e21d6a642f55317b0680
Kai Wang <kdwang0917@gmail.com>
2023-01-14 16:28:17 -0500

    fix video detection

---
+++ videocapture.py
@@ -0,0 +1,35 @@
+import cv2
+from videodetection_modules.leftframe import leftframe
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+
+
+def main():
+    while True:
+        # Read the frame
+        _, img = cap.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.2, 1)
+
+        leftframe(faces)
+
+        # for (x, y, w, h) in faces:
+        #     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # cv2.imshow('img',img)
+
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k==27:
+            break
+    # Release the VideoCapture object
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ videodetection_modules/leftframe.py
@@ -0,0 +1,7 @@
+def leftframe(faces):
+    if (type(faces) == tuple):
+        print("where r u")
+        return True
+    else:
+        print(faces)
+        return False
