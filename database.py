b5f0d6fc697f1a13fd78bf4ed10e74ed3b4f65b1
Kai Wang <kdwang0917@gmail.com>
2023-01-14 22:07:38 -0500

    clean up code a bit

--- database.py
+++ database.py
@@ -1,19 +1,17 @@
-#database
-
 from time import time
 
 class StudySession:
     def __init__(self): 
         #only run first time
-        self.startTime  = time()
-        self.time =0
+        self.startTime = time()
+        self.time = 0
         self.timesAFK = 0
         #can add modules here
     
     def finished(self, AFKcounter):
         #session is finished, record needed info to the study session
         self.endTime = time()
-        self.time = self.endTime -self.startTime 
+        self.time = self.endTime - self.startTime 
         self.timesAFK = AFKcounter #record times AFK to the study session
     
     def getStartTime(self):
@@ -21,4 +19,4 @@ class StudySession:
         return self.startTime
     def output(self):
         #output parameters to terminal, used for testing/debugging pruposes
-         print(f'Start Time: {self.startTime}, End Time: {self.endTime}, Session Duration: {self.time}, times AFK {self.timesAFK}')
+        print(f'Start Time: {self.startTime}, End Time: {self.endTime}, Session Duration: {self.time}, times AFK {self.timesAFK}')
--- videocapture.py
+++ videocapture.py
@@ -1,18 +1,17 @@
 import cv2
 import time
-#from videodetection_modules.leftframe import leftframe
 import database #get classes defined from our database file
+
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # To capture video from webcam. 
 cap = cv2.VideoCapture(0)
 
-
-
 isStudying = False
 
 startPressed = True #will be bound to GUI button later, set to True for testing
+
 def main():
     global isStudying #boolean to test if in session
     global currentSession
@@ -21,48 +20,41 @@ def main():
     if startPressed: #start button pressed
         isStudying = True
     
-    
-    
-        
     Studying(currentSession, currentSession.getStartTime())
     
-    
-    
-    
     #exitting loop means stop pressed-> need to reset varriables and track data
-    AFKcouner = 0
+
 def Studying(currentSession, start_time): #once study session is started (start button pressed)
     global isStudying 
     left_time = start_time
-
     
     session = database.StudySession()
     session = currentSession
     
-    AFKcounter= 0 
+    AFKcounter = 0 
     x = 20
-    timeout = time.time()+x
+    timeout = time.time() + x
     
-   
     toggle = False #toggles between gone and not gone. When we go from True(gone) to False(present), AFK counter goes up
     while isStudying:
        
-        if time.time()>timeout:
         #timer for testing - simulates someone pressing stop after x seconds (will be swapped to button later so dw about this)
-            isStudying=False#stop studying loop
-            
+        if time.time() > timeout:
+            isStudying = False#stop studying loop
         
         # Read the frame
         _, img = cap.read()
+
         # Convert to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # Detect the faces
+
+        # Detect face
         faces = face_cascade.detectMultiScale(gray, 1.2, 1)
        
         #atDesk: true = at desk, false = not at desk (past 10 seconds buffer)
         if (type(faces) == tuple): 
-            if (time.time() - left_time >3):
-                atDesk= False #left 
+            if (time.time()-left_time > 3):
+                atDesk = False #left 
             else:
                 atDesk =  True
         
@@ -70,38 +62,24 @@ def Studying(currentSession, start_time): #once study session is started (start 
             left_time = time.time()
             atDesk = True
         
-      
-        
-      
-        
         if (atDesk):
             toggle = False #present at desk
             print(faces)
         else:
             if (toggle == False):
-                AFKcounter+=1 #we have changed from present to gone
+                AFKcounter += 1 #we have changed from present to gone
             toggle = True #gone
             print("gone")
         
             #start_time = time.time()
-
-
-        k = cv2.waitKey(30) & 0xff
-        if k==27:
-            break
         
     # Release the VideoCapture object
-
     cap.release()
     cv2.destroyAllWindows()
-        
         
     #excited while loop -> studying is done  time to record data
     currentSession.finished(AFKcounter) 
     currentSession.output()#testing purposes: outputs data to terminal 
-        
-    
-
 
 if __name__ == "__main__":
     main()
