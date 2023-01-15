5f4ef4172e69909172b9f1b2e5336705f339a5c3
Kai Wang <kdwang0917@gmail.com>
2023-01-15 00:04:28 -0500

    add startstop button logic

--- GUI.py
+++ GUI.py
@@ -5,28 +5,35 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout,
 #from layout_colorwidget import Color
 
 # Subclass QMainWindow to customize your application's main window
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(600,400)
         self.setWindowTitle("StudyCam")
 
-        self.setWindowTitle("My App")
         layout = QVBoxLayout()
+        self.setLayout(layout)
 
-        startstop = QPushButton("Start", self)
-        startstop.clicked.connect(self.updateStartStop)
+        self.startstop = QPushButton("Start")
+        self.startstop.clicked.connect(self.updateStartStop)
+        layout.addWidget(self.startstop)
 
-        drinkwater = QPushButton("Drink water = true", self)
+        drinkwater = QPushButton("Drink water = true")
 
-        pause = QPushButton("Pause", self)
+        pause = QPushButton("Pause")
+        layout.addWidget(drinkwater)
+        layout.addWidget(pause)
 
-        startstop.move(50,50)
-        drinkwater.move(250,50)
-        pause.move(50,250)
+
+        # startstop.move(50,50)
+        # drinkwater.move(250,50)
+        # pause.move(50,250)
 
     def updateStartStop(self):
-        print("hi")
+        if (self.startstop.text() == "Start"):
+            self.startstop.setText("Stop")
+        else:
+            self.startstop.setText("Start")
 
 app = QApplication(sys.argv)
 window = MainWindow()
